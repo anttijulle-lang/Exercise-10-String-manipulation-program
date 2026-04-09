e620d84d41f8f07bc260774b4aede35493f68e35
Antti Järlström <antti.julle@gmail.com>
2026-04-09 17:49:05 +0300

    display_menu function added

--- project.c
+++ project.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+void display_menu(void);
+
 int main() {
-    printf("String manipulation program\n");
+    display_menu();
     return 0;
 }
- 
+
+void display_menu(void) {
+    printf("A) Count the number of vowels in the string\n");
+    printf("B) Count the number of consonants in the string\n");
+    printf("C) Convert the string to uppercase\n");
+    printf("D) Convert the string to lowercase\n");
+    printf("E) Display the current string\n");
+    printf("F) Enter another string\n");
+    printf("G) Read string from file\n");
+    printf("H) Write string to file\n\n");
+    printf("M) Display this menu\n");
+    printf("X) Exit the program\n");
+}
